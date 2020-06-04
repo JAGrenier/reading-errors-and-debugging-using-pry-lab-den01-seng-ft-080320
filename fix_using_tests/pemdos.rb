43e6a8e75ff9609f3dd53ae23d525d88699c82c7
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-04 17:14:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,10 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+  "s" + string * 10 
   else
     string
   end
 end
-binding.pry
+
